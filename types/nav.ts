efd75a3aeaa366e29dd93f25a27a350365f9a3cb
Artur Klesun <arturklesun@gmail.com>
2024-07-25 15:11:03 +0300

    Add constructor for CrewCode type

--- types/nav.ts
+++ types/nav.ts
@@ -1,18 +1,20 @@
 /**
  * @package -  here you'll find types related to our NAV 2009 software integration
- */ import type { AbsoluteMonth } from "../src/dates";
-import type { Brand } from "./utility";
-
-
-export type PayrollLocator = AbsoluteMonth & {
-    company: CompanyName,
-};
+ */ import type { Brand,CurrencyIsoCode } from "./utility";
 
 const UpperCased = Symbol("UpperCased");
 export type UpperCased<T> = T & { [k in typeof UpperCased]: undefined };
 
+// `| string` because otherwise "TS2590: Expression produces a union type that is too complex to represent."
+type Letter = "Q"|"W"|"E"|"R"|"T"|"Y"|"U"|"I"|"O"|"P"|"A"|"S"|"D"|"F"|"G"|"H"|"J"|"K"|"L"|"Z"|"X"|"C"|"V"|"B"|"N"|"M" | string;
+type Digit = "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9" | string;
+type AlphaNum = Letter | Digit;
+
+type PartnerCompanyCode = "F2R" | "HEL" | "D2W" | "APM" | `${AlphaNum}${AlphaNum}${AlphaNum}`;
+type ClientCode = "AAI" | "LAL" | "F2R" | `${AlphaNum}${AlphaNum}${AlphaNum}`;
+
 const CompanyNameTag = Symbol("CompanyName");
-export type CompanyName = Brand<"Airborne - Malta" | "HEL - AAIUSD" | "F2R - LALEUR" | "F2R - SLYEUR" | "F2R - DATEUR" | string, typeof CompanyNameTag>;
+export type CompanyName = Brand<"Airborne - Malta" | "HEL - AAIUSD" | "F2R - LALEUR" | "F2R - SLYEUR" | "F2R - DATEUR" | `${PartnerCompanyCode} - ${ClientCode}${CurrencyIsoCode}`, typeof CompanyNameTag>;
 export type COMPANYNAME = UpperCased<CompanyName>;
 
 /**
@@ -22,6 +24,20 @@ export type COMPANYNAME = UpperCased<CompanyName>;
  * aka Kennitala
  * aka Payroll No
  */
-const CrewCode = Symbol("CrewCode");
-export type CrewCode = Brand<"MVS" | "KAR" | "GIRE" | "UPC" | "JNM" | "AAD" | "klar" | string, typeof CrewCode>;
+const CrewCodeTag = Symbol("CrewCode");
+export type CrewCode = Brand<"MVS" | "KAR" | "GIRE" | "UPC" | "JNM" | "AAD" | "klar" | string, typeof CrewCodeTag>;
+export const CrewCode = (value: string): CrewCode => {
+    value = value.trim().toUpperCase();
+    if (value.length < 3) {
+        throw new Error("CrewCode can not be less than 3 characters");
+    }
+    if (value.length > 6) {
+        throw new Error("CrewCode is not expected to be longer than 6 characters");
+    }
+    if (!value.match(/^[A-Z]+$/)) {
+        throw new Error("CrewCode is expected to consist only of letters");
+    }
+    return value as CrewCode;
+};
+
 export type CREWCODE = UpperCased<CrewCode>;
