85e30cc85cc1ef5288b0e31ee437e24305134f3f
Artur Klesun <arturklesun@gmail.com>
2024-07-25 16:31:56 +0300

    Remove template literal from CompanyName type as it makes compiler go crazy on plain string comparisons

--- types/nav.ts
+++ types/nav.ts
@@ -1,20 +1,12 @@
 /**
  * @package -  here you'll find types related to our NAV 2009 software integration
- */ import type { Brand,CurrencyIsoCode } from "./utility";
+ */ import type { Brand } from "./utility";
 
 const UpperCased = Symbol("UpperCased");
 export type UpperCased<T> = T & { [k in typeof UpperCased]: undefined };
 
-// `| string` because otherwise "TS2590: Expression produces a union type that is too complex to represent."
-type Letter = "Q"|"W"|"E"|"R"|"T"|"Y"|"U"|"I"|"O"|"P"|"A"|"S"|"D"|"F"|"G"|"H"|"J"|"K"|"L"|"Z"|"X"|"C"|"V"|"B"|"N"|"M" | string;
-type Digit = "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9" | string;
-type AlphaNum = Letter | Digit;
-
-type PartnerCompanyCode = "F2R" | "HEL" | "D2W" | "APM" | `${AlphaNum}${AlphaNum}${AlphaNum}`;
-type ClientCode = "AAI" | "LAL" | "F2R" | `${AlphaNum}${AlphaNum}${AlphaNum}`;
-
 const CompanyNameTag = Symbol("CompanyName");
-type CompanyNameLiteral = "Airborne - Malta" | "HEL - AAIUSD" | "F2R - LALEUR" | "F2R - SLYEUR" | "F2R - DATEUR" | `${PartnerCompanyCode} - ${ClientCode}${CurrencyIsoCode | string}`;
+type CompanyNameLiteral = "Airborne - Malta" | "HEL - AAIUSD" | "F2R - LALEUR" | "F2R - SLYEUR" | "F2R - DATEUR" | string;
 export type CompanyName = Brand<CompanyNameLiteral, typeof CompanyNameTag>;
 export const CompanyName = (value: string): CompanyName => {
     if (!value.match(/^\w+\s*-\s*\w+$/)) {
