177bc45fc66f9bd1a54d2485a688392d496ebbc5
Artur Klesun <arturklesun@gmail.com>
2025-05-16 11:39:23 +0000

    Fix `WeakMap key Symbol(Passport) must be an object` error happening on firefox

--- src/utils/ReferenceUid.ts
+++ src/utils/ReferenceUid.ts
@@ -5,13 +5,28 @@
 
 let lastUid = 0;
 const refToUid = new WeakMap<object | symbol, number>;
+// firefox does not support symbols as weak map keys without a flag
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1710433
+const refToUidPermanent = new Map<object | symbol, number>;
 
 export function getRefUid(reference: object | symbol) {
-    const existingUid = refToUid.get(reference);
+    const existingUid = refToUid.get(reference)
+        ?? refToUidPermanent.get(reference);
     if (existingUid) {
         return existingUid;
     }
     const newUid = ++lastUid;
-    refToUid.set(reference, newUid);
+    try {
+        refToUid.set(reference, newUid);
+    } catch (error) {
+        if (typeof reference === "symbol" &&
+            error instanceof TypeError &&
+            error.message.includes("must be an object")
+        ) {
+            refToUidPermanent.set(reference, newUid);
+        } else {
+            throw error;
+        }
+    }
     return newUid;
 }
