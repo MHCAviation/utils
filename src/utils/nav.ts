b98996fa9431da9adecfe64e96bb459147b6f7f6
lauris <la.stepanovs@gmail.com>
2025-08-25 18:41:04 +0300

    Add parseCOMPANYNAME helper function

---
+++ src/utils/nav.ts
@@ -0,0 +1,20 @@
+import type { CompanyName } from "../../types/nav.ts";
+
+export function parseCOMPANYNAME(COMPANYNAME: CompanyName) {
+    if (COMPANYNAME.toUpperCase() === "AIRBORNE - MALTA") {
+        return {
+            partnerCompany: "APM",
+            client: "AAI",
+            currency: "USD",
+        };
+    }
+    // CRM IR - ABBEUR
+    // F2R - EAFEUR
+    // Airborne - Malta
+    const match = COMPANYNAME.match(/^(\w*\s*\w+)\s*-\s*(\w+)([A-Z]{3})$/);
+    if (!match) {
+        return null;
+    }
+    const [, partnerCompany, client, currency] = match;
+    return { partnerCompany, client, currency };
+}
