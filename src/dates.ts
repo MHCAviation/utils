34058272ee2aa58fe530bb5d67f3be63b317ecb0
Artur Klesun <arturklesun@gmail.com>
2025-08-27 11:07:04 +0000

    Add getMonthFromShortName() function

--- src/dates.ts
+++ src/dates.ts
@@ -126,12 +126,28 @@ export function getWeekDayName(weekDay: number) {
     return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][weekDay];
 }
 
-export function getMonthShortName(month: number) {
-    return [
-        "Jan", "Feb", "Mar", "Apr",
-        "May", "Jun", "Jul", "Aug",
-        "Sep", "Oct", "Nov", "Dec",
-    ][month - 1];
+const MONTH_SHORT_NAMES = [
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec",
+] as const;
+
+export type MonthShortName = (typeof MONTH_SHORT_NAMES)[number];
+
+export function getMonthShortName(month: number): MonthShortName {
+    return MONTH_SHORT_NAMES[month - 1];
+}
+
+/** @return 1-12 */
+export function getMonthFromShortName(shortName: MonthShortName | string): number {
+    const index = MONTH_SHORT_NAMES
+        .map(cs => cs.toUpperCase())
+        .indexOf(shortName.toUpperCase());
+    if (index) {
+        return index + 1;
+    } else {
+        throw new Error("Unexpected month short name: " + shortName);
+    }
 }
 
 export function getMonthFullName(month: number) {
